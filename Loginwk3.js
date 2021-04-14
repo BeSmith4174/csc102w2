4db148cf2334a3d5c1edde59b3d7c5cea2041c29
BeSmith4174 <82059759+BeSmith4174@users.noreply.github.com>
2021-04-14 13:19:55 -0500

    Add files via upload

--- Loginwk3.js
+++ Loginwk3.js
@@ -5,21 +5,24 @@ function checkCreds() {
     var lastName = document.getElementById("lName").value;
     //place to store badge id
     var badgeNumb = document.getElementById("badgeID").value;
-
+//combines first and last name
     var fullName = firstName + " " + lastName;
-
+//establishes character length allowed
     if (fullName.length > 20 || fullName == 1) {
-
+//this is an alert shown for an invalid entry
         document.getElementById("loginStatus").innerHTML = "Invalid full name!";
     }
-
-    if (badgeNumb > 999 || badgeNumb < 1) {
+//sets max allowable numbers entered
+    else if (badgeNumb > 999 || badgeNumb < 1) {
+        //this alerts user to an invalid entry
         document.getElementById("loginStatus").innerHTML = "Invalid badge number!";
 
     }
-
-    if(fullName.length<21 && fullName.length > 1 && badgeNumb < 1000 && badgeNumb > 0 ){
+//means if all entries are valid 
+    else{
+        //this is the alert upon succesful entry of all info
 alert("Access Granted! Welcome! " + fullName);
+//this redirects you to the space webpage
 location.replace("HTMLW2.html")
     }
 
